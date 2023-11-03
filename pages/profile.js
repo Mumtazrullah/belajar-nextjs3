10f23de4d76aed4628672ee3595de85cea7fc493
Mumtazrullah <anwarullahmumtaz404@gmail.com>
2023-11-03 12:54:18 +0700

    ubah dan tambah

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+const profile =() => {
+    return (
+        <>
+        <Navbar />
+        <h1>Ini Halaman Profile</h1>
+        </>
+    )
+};
+
+export default profile;
